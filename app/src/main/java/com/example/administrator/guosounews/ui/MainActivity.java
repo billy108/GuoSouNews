256407a241855e72c341f7526df901c0ad21d843
billy <billy_108@163.com>
2016-05-12 11:03:05 +0800

    3 skip to MainActivity or GuidieActivity

---
+++ app/src/main/java/com/example/administrator/guosounews/ui/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.administrator.guosounews.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
